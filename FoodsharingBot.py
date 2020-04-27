00cf50b3c76f804170ac3fecf6f0eb1ad3597e6b
Tam <tames98@gmail.com>
2020-04-27 20:35:31 +0700

    road to MVP

--- FoodsharingBot.py
+++ FoodsharingBot.py
@@ -9,18 +9,17 @@ f.close()
 
 bot = telebot.TeleBot(TOKEN)
 
-keyboard_main = telebot.types.ReplyKeyboardMarkup()
-keyboard_main.row('Поделиться', 'Забрать')
-
-keyboard_main.resize_keyboard = True
-
-#telebot.types.
-
-touch_count = 0
+@bot.message_handler(commands=['test'])
+def test_message(message):
+    bot.send_location(message.chat.id, 56.018012, 92.868991)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет! Добро пожаловать в мир рационального использования ресурсов\nВыбери действие',  reply_markup=keyboard_main)
+    keyboard_main = telebot.types.ReplyKeyboardMarkup()
+    keyboard_main.row('Правила', 'Главное меню','Поделиться', 'Забрать')
+    keyboard_main.resize_keyboard = True
+
+    bot.send_message(message.chat.id, 'Привет! Добро пожаловать в мир рационального использования ресурсов\nВыбери действие', reply_markup=keyboard_main)
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
@@ -30,20 +29,32 @@ def help_message(message):
 def settings_message(message):
     bot.send_message(message.chat.id, 'not implemented')
 
+# @bot.inline_handler(lambda query: query.query == 'text')
+# def query_text(inline_query):
+
+
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     chat_id = message.chat.id
-    if message.text == 'тык':
-        global touch_count
-        touch_count = touch_count + 1
-        bot.send_message(message.chat.id, 'тык'*touch_count)
-    elif message.text == 'Поделиться':
+    if message.text == 'Поделиться':
         bot.send_message(chat_id, 'Введите продукты')
     elif message.text == 'Забрать':
         pass
+    elif message.text == 'Правила':
+        pass
+    elif message.text == 'Главное меню':
+        markup_inline = telebot.types.InlineKeyboardMarkup()
+    
+        button_map = telebot.types.InlineKeyboardButton('Карта доноров', url='https://2gis.ru/krasnoyarsk/firm/986145966616730?m=92.797081%2C55.994433%2F16')
+        # button_rules = telebot.types.InlineKeyboardButton('Правила', callback_data='test')
+        
+        # markup_inline.add(button_rules)
+        markup_inline.add(button_map)
+
+        bot.send_message(message.chat.id, 'Главное меню', reply_markup=markup_inline)
     else:
-        bot.send_message(message.chat.id, message.text)
+        bot.send_message(message.chat.id, 'Я тебя не понимаю :(')
 
 @bot.message_handler(content_types=['voice'])
 def handle_voice(message):
@@ -58,7 +69,10 @@ def handle_voice(message):
 
     bot.send_voice(message.chat.id, voice_file.content)
     bot.send_message(message.chat.id, message)
+    
+bot.polling()
 
+'''
 while True:
     try:
         bot.polling(none_stop=True)
@@ -67,3 +81,4 @@ while True:
         print('some problems')
         time.sleep(5)
 #bot.infinity_polling()
+'''
