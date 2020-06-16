bff4d53a077e6cecf054bf43fde3cde81fb465f4
Tam <tames98@gmail.com>
2020-06-17 01:28:43 +0700

    db_export added

---
+++ db_export.sql
@@ -0,0 +1,21 @@
+BEGIN TRANSACTION;
+DROP TABLE IF EXISTS "offers";
+CREATE TABLE IF NOT EXISTS "offers" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"user_id"	INTEGER,
+	"name"	TEXT NOT NULL,
+	"description"	TEXT,
+	"marker_latitude"	REAL,
+	"marker_longitude"	REAL,
+	"is_complete"	INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("user_id") REFERENCES "users"("id")
+);
+DROP TABLE IF EXISTS "users";
+CREATE TABLE IF NOT EXISTS "users" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT,
+	"phone"	TEXT,
+	PRIMARY KEY("id")
+);
+COMMIT;
